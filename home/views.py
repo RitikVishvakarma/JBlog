2539b6d9f46a3f68261124bb96736af5177212ab
Ritik <ritikvishvakarma2000@gmail.com>
2020-12-17 11:08:10 +0530

    Messages Framework, message flashing in Django

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -9,12 +10,17 @@ def about(request):
     return render(request, 'home/about.html')
 
 def contact(request):
+    # messages.success(request, 'Welcome to contact.')
     if request.method=='POST':
         name = request.POST['name']
         email = request.POST['email']
         phone = request.POST['phone']
         content = request.POST['content']
         print(name, email, phone, content)
-        contact = Contact(name=name, email=email, phone=phone, content= content)
-        contact.save()
+        if len(name)<2 or len(email)<3 or len(phone)<10 or len(content)<4:
+            messages.error(request, 'Please fill the form correctly')
+        else:
+            contact = Contact(name=name, email=email, phone=phone, content= content)
+            contact.save()
+            messages.success(request, "Your message has been successfully sent")
     return render(request, 'home/contact.html')
